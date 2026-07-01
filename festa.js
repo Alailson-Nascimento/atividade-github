28e4bbf2dd32651b50bf8ff451be44e7a84a5b46
Alailson Nascimento <alailsonnascimento707@gmail.com>
2026-06-30 21:10:57 -0300

    adiciona exercicio de JS basico com operador ternario

---
+++ festa.js
@@ -0,0 +1,9 @@
+// Definindo a variavel com let
+let idade = 20;
+
+// Usando o operador ternário
+
+let resultado = idade >= 18 ? "Pode entrar na festa" : "Não pode entrar";
+
+// Exibindo resultado no console
+ console.log(resultado);
